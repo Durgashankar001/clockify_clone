551713534ad48b0631e8ab355d86c0b5d92b4629
sks-7 <sachinsinghofficial7@gmail.com>
2022-09-28 20:46:22 +0530

    Day-2 task completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-     {/* <Navbar/> 
-     <HomePage/>  */}
+     <Navbar/> 
+     <HomePage/> 
       <FilterSection/>
     </div>
   );
