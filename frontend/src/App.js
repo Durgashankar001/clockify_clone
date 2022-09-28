5f2e879530bb4382668ed1e2b87f31d2bcf28881
Durgashankar Pal <durgashankarpal14285@gmail.com>
2022-09-28 12:17:23 +0530

    Home Page Content first half done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import HomePage from './Components/HomePage/HomePage';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     HAPPY HACKING!
+     <Navbar/>
+     <HomePage/>
     </div>
   );
 }
