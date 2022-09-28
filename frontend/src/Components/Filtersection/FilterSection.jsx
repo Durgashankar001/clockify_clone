9be2023f63dc6344cc45845ec07cbf30405f69e7
sks-7 <sachinsinghofficial7@gmail.com>
2022-09-28 20:41:43 +0530

    Day-2 task completed

--- frontend/src/Components/Filtersection/FilterSection.jsx
+++ frontend/src/Components/Filtersection/FilterSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './filterswection.css';
 import { FiSearch } from 'react-icons/fi';
-import { Button, Checkbox, Input, Portal } from '@chakra-ui/react';
+import { Button, Checkbox, Input, Portal, Text } from '@chakra-ui/react';
 import {
   Popover,
   PopoverTrigger,
@@ -20,7 +20,7 @@ const FilterSection = () => {
       <div className="container">
         <table width="50%">
           <tr>
-            <th>Filter</th>|
+            <th>Filter</th>
             <th>
               <select name="" id="">
                 <option>Active</option>
@@ -28,7 +28,7 @@ const FilterSection = () => {
                 <option>All</option>
               </select>
             </th>
-            |
+
             <th>
               {/* <select name="" id="">
                 <option>Client</option>
@@ -42,9 +42,13 @@ const FilterSection = () => {
                 <option>Active</option>
               </select> */}
 
+              {/*  chakra ui used*/}
+
               <Popover>
                 <PopoverTrigger>
-                  <Button bg={"none"} _hover={{background:"none"}}>Client</Button>
+                  <Button bg={'none'} _hover={{ background: 'none' }}>
+                    Client
+                  </Button>
                 </PopoverTrigger>
                 <Portal>
                   <PopoverContent>
@@ -52,24 +56,56 @@ const FilterSection = () => {
                     <PopoverHeader>Clints</PopoverHeader>
                     <PopoverCloseButton />
                     <PopoverBody>
-                      <Input mt={5} mb={5} />
+                      <Input mt={5} mb={5} placeholder="Search client" />
                     </PopoverBody>
-                    <PopoverFooter>This is the footer</PopoverFooter>
+                    <PopoverFooter>No clients left</PopoverFooter>
                   </PopoverContent>
                 </Portal>
               </Popover>
             </th>
-            |
+
             <th>
-              <select name="" id="">
+              {/* <select name="" id="">
                 <option>Access</option>
                 <option>Active</option>
                 <option>Active</option>
-              </select>
+              </select> */}
+
+              {/*  chakra ui used*/}
+
+              <Popover>
+                <PopoverTrigger>
+                  <Button bg={'none'} _hover={{ background: 'none' }}>
+                    Access
+                  </Button>
+                </PopoverTrigger>
+                <Portal>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverHeader>Access</PopoverHeader>
+                    <PopoverCloseButton />
+                    <PopoverBody>
+                      <Input
+                        mt={5}
+                        mb={5}
+                        placeholder="Find members of groups..."
+                      />
+                      <Checkbox>Select All</Checkbox>
+                    </PopoverBody>
+
+                    <PopoverFooter>
+                      <Text fontSize="xl" mb={2}>
+                        Users
+                      </Text>
+                      <Checkbox>Users</Checkbox>
+                    </PopoverFooter>
+                  </PopoverContent>
+                </Portal>
+              </Popover>
             </th>
-            |
+
             <th>
-              <select name="" id="">
+              {/* <select name="" id="">
                 <option>Billing</option>
                 <option>
                   <Checkbox className="check" defaultChecked>
@@ -79,7 +115,29 @@ const FilterSection = () => {
                 <option>
                   <Checkbox defaultChecked> Non billable</Checkbox>
                 </option>
-              </select>
+              </select> */}
+
+              {/*  chakra ui used*/}
+
+              <Popover>
+                <PopoverTrigger>
+                  <Button bg={'none'} _hover={{ background: 'none' }}>
+                    Billing
+                  </Button>
+                </PopoverTrigger>
+                <Portal>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverHeader>Billing</PopoverHeader>
+                    <PopoverCloseButton />
+                    <PopoverBody>
+                      <Checkbox mr={3}>Billable</Checkbox>
+                      <Checkbox>Non billable</Checkbox>
+                    </PopoverBody>
+                    <PopoverFooter></PopoverFooter>
+                  </PopoverContent>
+                </Portal>
+              </Popover>
             </th>
           </tr>
         </table>
