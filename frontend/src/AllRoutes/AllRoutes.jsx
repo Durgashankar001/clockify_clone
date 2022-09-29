915492987c4a3006f2b28a1dcb578dcaa7f7c565
shubhamgohad221997 <shubhamgohad221997@gmail.com>
2022-09-29 15:31:23 +0530

    Routes_Add

--- frontend/src/AllRoutes/AllRoutes.jsx
+++ frontend/src/AllRoutes/AllRoutes.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Download from '../Components/DownloadPage/Download'
+import Features from '../Components/FeaturesPage/FeaturesPage'
 import FeaturesPage from '../Components/FeaturesPage/FeaturesPage'
+import FilterSection from '../Components/Filtersection/FilterSection'
 import Footer from '../Components/Footer/Footer'
 import HomePage from '../Components/HomePage/HomePage'
 import Navbar from '../Components/Navbar/Navbar'
 import Signup from '../Components/SignupPage/Signup'
+import Time_Tracker from '../Components/Time_Tracker/Time_Tracker'
 
 const AllRoutes = () => {
 
@@ -40,6 +43,9 @@ const AllRoutes = () => {
           }
         ></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/tracker" element={<Time_Tracker/>}></Route>
+        <Route path="/features" element={<Features/>}></Route>
+        <Route path="/project" element={<FilterSection/>}></Route>
      </Routes>
     </>
   )
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
      <AllRoutes/>
-    </div>
+     </div>
   );
 }
 
