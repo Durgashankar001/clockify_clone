86d3179e058271c82f0b0f8be138542d7495c3a6
Durgashankar Pal <durgashankarpal14285@gmail.com>
2022-09-30 12:20:15 +0530

    backend done

--- backend/Controller/project.router.js
+++ backend/Controller/project.router.js
@@ -17,3 +17,46 @@ projectControl.get("/", async (req, res) => {
         res.status(500).send("Internal Server Error")
     }
 })
+
+
+projectControl.post("/new",async(req,res)=>{
+    try{
+       const newProject = await ProjectModel.create(req.body) 
+       return res.status(200).send({message:"Project added successfully",newProject})
+    }catch(err)
+    {
+        return res.status(500).send("Project added failed")
+    }
+})
+
+projectControl.patch("/:id",async(req,res)=>{
+    const {id} = req.params
+    try{
+        const updatedProject = await ProjectModel.findByIdAndUpdate({_id:id},req.body,{
+            new:true
+        })
+
+        return res.status(200).send({message:"Project Updated",updatedProject})
+
+
+    }catch(err)
+    {
+        return res.status(500).send("Internal server error")
+    }
+})
+
+projectControl.delete("/:id",async(req,res)=>{
+    const {id}  = req.params
+    try{
+        await ProjectModel.findByIdAndDelete({_id:id})
+        res.status(200).send({message:"Deleted successfully"})
+
+    }catch(err){
+        return res.status(500).send("Internal server error")
+    }
+})
+
+
+module.exports = projectControl
+
+
--- backend/Models/project.model.js
+++ backend/Models/project.model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
 const projectSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: false },
     tag: {type: String},
-    billable: { type: Boolean, required: true },
-    useremail: { type: String, required: true },
+    billable: { type: Boolean, required: false },
+    useremail: { type: String, required: false },
 })
 
 const ProjectModel = mongoose.model("project",projectSchema)
