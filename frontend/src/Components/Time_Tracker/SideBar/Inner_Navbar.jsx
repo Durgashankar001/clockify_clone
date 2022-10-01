219c863c4fd00c14b70964b926691a48d973fef0
shubhamgohad221997 <shubhamgohad221997@gmail.com>
2022-10-01 09:18:31 +0530

    Day_5_Add

---
+++ frontend/src/Components/Time_Tracker/SideBar/Inner_Navbar.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import "./sidebar.css";
+
+const Inner_Navbar = () => {
+  return (
+    <div>
+   <Box className='mainNav'>
+        <Box></Box>
+        <Box></Box>
+   </Box>
+    </div>
+  )
+}
+
+export default Inner_Navbar
--- frontend/src/Components/Time_Tracker/Time_Tracker.jsx
+++ frontend/src/Components/Time_Tracker/Time_Tracker.jsx
@@ -1,11 +1,16 @@
 import { Box, Menu } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import StopWatch from './StopWatch';
 import "./Timer.css";
-import { FaPlusCircle,FaStopwatch,FaCommentDots,FaDollarSign,FaTags,FaGripLinesVertical } from 'react-icons/fa';
+import { FaPlusCircle,FaStopwatch,FaCommentDots,FaDollarSign,FaTags,FaGripLinesVertical,FaRegCalendarAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
+
 import SideBar from './SideBar/SideBar';
+import Inner_Navbar from './SideBar/Inner_Navbar';
+import axios from 'axios';
+import { useState } from 'react';
+import { GrPlay } from "react-icons/gr";
+import { HiDotsVertical } from "react-icons/hi";
 
 
 const Time_Tracker = () => {
@@ -17,11 +22,28 @@ const Time_Tracker = () => {
 
   function taskadd(){
     navigate("/taskadd")
+
+
   }
+
+
+  const getData = async () => {
+    let res = await axios.get('http://localhost:8080/task');
+  
+    return res.data;
+  };
+  
+  const [data,setData]=useState([])
+  useEffect(()=>{
+    getData().then((res) => {
+      setData(res);
+  
+    });
+  },[])
 
   return (
     <div >
-      <Box><Navbar/></Box>
+      <Box><Inner_Navbar/></Box>
       <Box className='timePage'>
         <Box className='st01'>
 <SideBar/>
@@ -41,6 +63,48 @@ const Time_Tracker = () => {
         <div  className='wname'><h1 className='h1'>This week</h1> <h1 className='h1'>Week Total:00:00:00</h1></div>
         <div  className='wname wname1'><h1 className='h1'>This week</h1> <h1 className='h1'>Week Total:00:00:00</h1></div>
 
+
+       {/* MapFunction */}
+       {
+        data.map((e)=>(
+        <Box className="st">
+          <div className="A1">
+            <h1>{e.name}</h1>
+          </div>
+          <div className="A2" >
+            <FaPlusCircle />
+            <a href="" className="pin">
+              Project
+            </a>
+          </div>
+          <div className="A3">
+            <FaGripLinesVertical className="hicon" />
+            <FaTags className="hicon" />
+            <FaGripLinesVertical className="hicon" />
+            <FaDollarSign className="hicon" />
+            <FaGripLinesVertical className="hicon" />
+            
+          </div>
+          <div className="A4"><h1>{e.startAt}</h1>-
+            <h1>{e.endAt}</h1></div>
+          <div className="A5">
+           
+            <FaRegCalendarAlt /> 
+          </div>
+          <div className="A6">
+         
+            <h1>{e.totalTime}</h1>
+            
+          </div>
+          <div className="A7">
+           <button><GrPlay/></button>
+          </div>
+
+          <div className="A7"><button><HiDotsVertical/></button></div>
+        
+        </Box>
+        ))
+      }
          </Box>
          </Box>
     </div>
